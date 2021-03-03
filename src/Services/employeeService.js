d49b3f2a55323dd8bf31fff6835e093f8545bc0e
Hari Mahat <hmahat@gmail.com>
2021-03-03 20:13:42 +0200

    Employee data seeding

---
+++ src/Services/employeeService.js
@@ -0,0 +1,6 @@
+export const getDepartmentCollection = () => [
+  { id: '1', title: 'Accounting' },
+  { id: '2', title: 'HR' },
+  { id: '3', title: 'Marketing' },
+  { id: '4', title: 'Development' },
+];
